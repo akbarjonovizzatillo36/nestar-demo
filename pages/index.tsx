b3534fe897d1ad1193c3ee25361e0ef83c53e20c
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-05-06 02:13:52 -0700

    fix: refactor pages components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,28 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import { Box, Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack flexDirection={"column"}>
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Agents</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Properties</Stack>
-        </Stack>
+    <Stack className={"home-page"}>
+      <Stack>
+        <Stack className="container">Trend Properties</Stack>
+      </Stack>
+
+      <Stack>
+        <Stack className="container">Popular Properties</Stack>
+      </Stack>
+
+      <Stack>
+        <Stack className="container">Advertisement</Stack>
+      </Stack>
+
+      <Stack>
+        <Stack className="container">Top Properties</Stack>
+      </Stack>
+
+      <Stack>
+        <Stack className="container">Top Agents</Stack>
       </Stack>
     </Stack>
   );
